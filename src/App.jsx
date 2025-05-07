b4094a358a2583978eb7e5d27081cd93f0095b6b
Tharushirdtk <kavindirdtk@gmail.com>
2025-05-07 20:43:04 +0530

    installing tailwind css 🧩

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ProductCard from './components/productCard'
-import UserData from './components/userData'
+import LoginPage from './pages/loginPage'
+import HomePage from './pages/homePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AdminHomePage from './pages/adminHomePage'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <div className='bg-primary'>
     <BrowserRouter>
     <Routes path="/*">          
     <Route path="/*" element={<HomePage/>}/>   
     <Route path="/login" element={<LoginPage/>}/>
     </Routes>
     </BrowserRouter>
+    </div>
     </>
   )
 }
