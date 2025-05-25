f3cce3dde76cc08874b790ee91ca3505653601e4
Tharushirdtk <kavindirdtk@gmail.com>
2025-05-25 23:53:51 +0530

    update mistakes

--- src/components/userData.jsx
+++ src/components/userData.jsx
@@ -2,10 +2,13 @@ export default function UserData(){
 
     return(
         <>
-          <img src="C:\Users\USER\Downloads\17578817411582823579-128.png"></img>
-              <span>Tharushi Kavindi</span>
-              <span>Online</span>
-        </>
+        
+        <img src="/path/to/image.jpg" alt="Image description" />
 
+        <span>Tharushi</span>
+        <span>Online</span>
+        </>
+        
     )
 }
+
