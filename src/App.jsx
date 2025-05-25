582a02e423fa6cf422ebe716f4d0842dc3054d3d
Tharushirdtk <kavindirdtk@gmail.com>
2025-05-25 23:07:51 +0530

    modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,40 @@
 import { useState } from 'react'
 import './App.css'
-import LoginPage from './pages/loginPage'
-import HomePage from './pages/homePage'
+import ProductCard from './components/productCard'
+import UserData from './components/userData'
+import Testing from './components/testing'
+import LoginPage from './components/pages/loginPage'
+import HomePage from './components/pages/homePage'
+import SignInPage from './components/pages/signInPage.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AdminHomePage from './pages/adminHomePage'
-
-
+import AdminHomePage from './components/pages/adminHomePage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <div className='bg-primary'>
-    <BrowserRouter>
-    <Routes path="/*">          
-    <Route path="/*" element={<HomePage/>}/>   
-    <Route path="/login" element={<LoginPage/>}/>
-    </Routes>
-    </BrowserRouter>
-    </div>
+      <BrowserRouter>
+        <Routes path="/*">
+          <Route path="/home" element={<HomePage/>}></Route>
+
+          <Route path="/login" element={<LoginPage/>}></Route>
+
+          <Route path='/*' element={<h1>404 error</h1>}></Route>
+
+          <Route path= "/signin" element={<signInPage/>}/>
+
+          <Route path="/admin/*" element={<AdminHomePage/>}/>
+        </Routes>
+
+       
+       
+
+      </BrowserRouter>
+      
+
     </>
   )
 }
 
-export default App
+export default App;
